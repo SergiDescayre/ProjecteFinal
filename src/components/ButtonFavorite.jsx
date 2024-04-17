197417abaf0098282ec6d7039579f83e96dfbc81
SergiDescayre <lepetitdesca@gmail.com>
2024-04-17 21:57:13 +0200

    add favorites with idFavorites

--- src/components/ButtonFavorite.jsx
+++ src/components/ButtonFavorite.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import heart from "../assets/heart.svg"
 import heartFavorite from "../assets/heartFavorite.svg"
 import appFirebase from "../credentials"
@@ -6,38 +6,33 @@ import { getAuth } from "firebase/auth"
 import { getFirestore,collection, addDoc, query, where, getDocs, deleteDoc} from "firebase/firestore"
 
 const ButtonFavorite = ({fest}) => {
+  const auth = getAuth(appFirebase).currentUser.uid
   const firestore = getFirestore(appFirebase)
     const [isFavorite, setIsFavorite] = useState(false)
-    const [existsFavorite, setExistsFavorites] = useState(false)
-
-    useEffect(()=> {
-      checkFavorite(fest.id)
-    },[])
-
-    console.log(fest.name + " " + existsFavorite)
   
     const handleFavorites =  (id) => {
       setIsFavorite(!isFavorite)
-   
       !isFavorite ? addFavorite() : deleteFavorite(id)
       }
     
-      const addFavorite = async () => {
+
+      const addFavorite = async (id) => {
         try {
           const db = getFirestore(appFirebase);
           await addDoc(collection(db, "favorites"), {
-            fest
+            ...fest, userIdFavorite:auth
           });
-          setExistsFavorites(true)
+          console.log("documento añadido con ID: "+ fest.id)
         } catch (error) {
           console.error("Error al agregar favorito:", error);
         }
       };
 
       const deleteFavorite = async (id) => {
+        console.log(id)
         try {
          
-          const q = query(collection(firestore, 'favorites'), where('fest.id', '==', id));
+          const q = query(collection(firestore, 'favorites'), where('id', '==', id));
   
           // Obtener documentos que cumplen con la condición
           const querySnapshot = await getDocs(q);
@@ -45,39 +40,16 @@ const ButtonFavorite = ({fest}) => {
           // Para cada documento encontrado, eliminarlo
             querySnapshot.forEach(async (doc) => {
             await deleteDoc(doc.ref);
-            setExistsFavorites(false)
             console.log(`Documento eliminado con ID: ${doc.id}`);
           });
         } catch (error) {
-          console.error('Error al eliminar favorito:', error);
-        }
-      };
-
-      const checkFavorite = async (id) => {
-        const auth = getAuth(appFirebase)
-        try {
-          // Consulta para buscar el documento en la colección de favoritos
-          const q = query(collection(firestore, 'favorites'), where('fest.userFavorite', '==', auth.currentUser.uid));
-  
-          // Obtener documentos que cumplen con la condición
-          const querySnapshot = await getDocs(q);
-  
-          // Verificar si la consulta devuelve algún resultado
-          if (!querySnapshot.empty) {
-            // El documento existe en la colección de favoritos
-            setExistsFavorites(true);
-          } else {
-            // El documento no existe en la colección de favoritos
-            setExistsFavorites(false);
-          }
-        } catch (error) {
-          console.error('Error al comprobar favorito:', error);
+          console.error('Error al eliminar documentos:', error);
         }
       };
   
   return (
     <div>
-      <img className="cursor-pointer" src={(!existsFavorite) ? heart : heartFavorite} alt="favorites" onClick={() => handleFavorites(fest.id)}/>
+      <img className="cursor-pointer" src={!isFavorite ? heart : heartFavorite} alt="favorites" onClick={() => handleFavorites(fest.id)}/>
     </div>
   )
 }
